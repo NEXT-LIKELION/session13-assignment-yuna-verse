79109dd0374493aee4f184f3c57fc63f3b8e6422
yuna-verse <yunha3528@naver.com>
2025-05-01 03:11:03 +0900

    과제 제출

---
+++ shopping-cart/src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CartPage from "./pages/CartPage";
+import ProductPage from "./pages/ProductPage";
+
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/product/:id" element={<ProductPage />} />
+                <Route path="/cart" element={<CartPage />} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
